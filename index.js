88315e4aeee07401c831c9cdfc83d93f86bbc5f6
daniel <jdanamarima@latam.stefanini.com>
2021-03-19 19:56:34 -0500

    Req-Dev: Se importa el modulo File System y se expone como trabaja de manera asincrona en nodejs

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-const math = require('./math.js');
-const os = require('os');
+const fs = require("fs");
 
-console.log(math.add(1,2));
-console.log(math.substract(1,2));
-console.log(math.multiply(1,2));
-console.log(math.divide(1,2));
-console.log(math.divide(1,0));
+fs.writeFile("./texto.txt", "linea uno", function (err) {
+  if (err) {
+    console.log(err);
+  }
+  console.log("Archivo creado");
+});
 
-console.log(os.platform());
-console.log(os.release());
-console.log('free mem: ', os.freemem(), ' bytes');
-console.log('total mem: ', os.totalmem(), ' bytes');
+console.log("Ultima linea de codigo");
