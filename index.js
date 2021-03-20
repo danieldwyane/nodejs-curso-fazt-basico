f066baedd229b21d06a1ab61aafc22d4a09256a5
daniel <jdanamarima@latam.stefanini.com>
2021-03-19 19:42:23 -0500

    Req-Dev: Se importa y se realizan ejemplos del modulo OS

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const math = require('./math.js');
+const os = require('os');
 
 console.log(math.add(1,2));
 console.log(math.substract(1,2));
 console.log(math.multiply(1,2));
 console.log(math.divide(1,2));
 console.log(math.divide(1,0));
+
+console.log(os.platform());
+console.log(os.release());
+console.log('free mem: ', os.freemem(), ' bytes');
+console.log('total mem: ', os.totalmem(), ' bytes');
