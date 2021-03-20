95aa13850c0ff0876d5a05386b41b0e6e0ff2d98
daniel <jdanamarima@latam.stefanini.com>
2021-03-19 20:30:05 -0500

    Req-Dev: Se utiliza el (npm install colors), para instalar la dependencia de colors para colores en la consola

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const http = require("http");
 
+const colors = require('colors');
+
 const handleServer = function (req, res) {
   res.writeHead(200, { "Content-type": "text/html" });
   res.write("<h1>Hola mundo</h1>");
@@ -9,5 +11,5 @@ const handleServer = function (req, res) {
 const server = http.createServer(handleServer);
 
 server.listen(3000, function () {
-  console.log("Server on port 3000");
+  console.log("Server on port 3000".green);
 });
